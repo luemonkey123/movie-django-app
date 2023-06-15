9520d28f488bd7708d2364948701f158e8503271
Evan Shen <bluemonkey3.1415926@gmail.com>
2023-06-15 14:43:26 -0400

    navbar and extending base templates

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html',  {'searchTerm': searchTerm})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html',  {'searchTerm': searchTerm, 'movies':movies})
 
 def about(request):
     return HttpResponse("<h1>Welcome to the About Page</h1>")
